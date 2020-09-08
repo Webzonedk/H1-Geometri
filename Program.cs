5a46eb0ca045632e1ca64b10dbec893545836891
Kent Pedersen <webzone@kpdesign.dk>
2020-09-08 12:02:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Geometri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square a = new Square(5);
+            Square b = new Square(10);
+            Square c = new Square(15);
+            Square d = new Square(20);
+            Square e = new Square(25);
+            a.CalcSides();
+            Console.WriteLine(a.CalcSides());
+            b.CalcSides();
+            Console.WriteLine(b.CalcSides());
+            c.CalcSides();
+            Console.WriteLine(c.CalcSides());
+            d.CalcSides();
+            Console.WriteLine(d.CalcSides());
+            e.CalcSides();
+            Console.WriteLine(e.CalcSides());
+            a.SideA = 253.2589;
+            a.CalcSides();
+            Console.WriteLine(a.CalcSides());
+            Console.WriteLine();
+
+            Console.WriteLine("Calculating area");
+            a.CalcArea();
+            Console.WriteLine(a.CalcArea());
+            b.CalcArea();
+            Console.WriteLine(b.CalcArea());
+            c.CalcArea();
+            Console.WriteLine(c.CalcArea());
+            d.CalcArea();
+            Console.WriteLine(d.CalcArea());
+            e.CalcArea();
+            Console.WriteLine(e.CalcArea());
+            a.SideA = 253.2589;
+            a.CalcArea();
+            Console.WriteLine(a.CalcArea());
+        }
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometri
+{
+    public class Square
+    {
+        #region Field
+        private double sideA;
+
+        #endregion
+
+
+
+        #region Proberties
+        public double SideA
+        {
+            get { return this.sideA; }
+            set { this.sideA = value; }
+        }
+
+        #endregion
+
+
+        #region Constructor
+        public Square(double sideA)
+        {
+            this.sideA = sideA;
+            // return sideA;
+        }
+        #endregion
+
+
+        #region Method
+        public double CalcSides()
+        {
+            double sidesTotal = sideA * 4;
+            return sidesTotal;
+        }
+
+        #endregion
+
+        #region Method
+        public double CalcArea()
+        {
+            double area = sideA * sideA;
+            return area;
+        }
+
+        #endregion
+
+    }
+}
